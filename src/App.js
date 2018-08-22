cb2c0c43061e2bd48df3cfb6f926e9a4721c3a02
Muigai <Muigai.Mwaura@Gmail.com>
2018-08-22 23:39:02 +0300

    Set state in event handler not on publish

--- src/App.js
+++ src/App.js
@@ -5,56 +5,55 @@ const scaleNames = {
   f: "Fahrenheit",
 };
 
+const toCelsius = (fahrenheit) => (fahrenheit - 32) * 5 / 9;
+
+const toFahrenheit = (celsius) => (celsius * 9 / 5) + 32;
+
+const temperatureChanged = "temperaturechanged";
+
 class TemperatureInput extends React.Component {
-
-  static temperatureChanged = "temperaturechanged";
-
-  toCelsius = (fahrenheit) => (fahrenheit - 32) * 5 / 9;
-
-  toFahrenheit = (celsius) => (celsius * 9 / 5) + 32;
-
-  isActiveInput = true;
-
-  getTemperature = () => this.isActiveInput ? this.state.temperature :
-                         isNaN(this.state.temperature) ? "" : this.state.temperature.toString();
 
   publishTemperatureChanged =
     (s) => {
 
-      this.setState({ temperature: s});
-
-      const temp = parseFloat(s);
-
       const data = {
-        celsius: this.props.scale === "c" ? temp : this.toCelsius(temp),
-        fahrenheit: this.props.scale === "f" ? temp : this.toFahrenheit(temp),
+        value: s,
         sourceScale: this.props.scale,
       };
 
-      publish(TemperatureInput.temperatureChanged, data);
+      publish(temperatureChanged, data);
     };
 
   onTemperatureChanged = (e) => {
-    if (this.props.scale !== e.sourceScale) {
-      this.isActiveInput = false;
-      this.setState({ temperature: this.props.scale === "c" ? e.celsius : e.fahrenheit });
-    }
-    else{
-      this.isActiveInput = true;
-    }
+
+    const scale = this.props.scale;
+
+    const temp = parseFloat(e.value);
+
+    let temperature =
+      (() => {
+        if (scale !== e.sourceScale) {
+          return isNaN(temp) ? "" : (scale === "c" ? toCelsius(temp) : toFahrenheit(temp))
+        }
+        else {
+          return e.value;
+        }
+      })();
+
+    this.setState({temperature});
   };
 
   render = () => (
     <fieldset>
       <legend>Enter temperature in {scaleNames[this.props.scale]}:</legend>
-      <input value={this.getTemperature()} onInput={(e) => this.publishTemperatureChanged(e.currentTarget.value)} />
+      <input value={this.state.temperature} onInput={(e) => this.publishTemperatureChanged(e.currentTarget.value)} />
     </fieldset>
   );
 
   constructor(props) {
     super(props);
     this.state = { temperature: "" };
-    subscribe(TemperatureInput.temperatureChanged, this.onTemperatureChanged);
+    subscribe(temperatureChanged, this.onTemperatureChanged);
   }
 }
 
@@ -62,12 +61,16 @@ class BoilingVerdict extends React.Component {
 
   onTemperatureChanged = (e) => {
 
-    if (isNaN(e.celsius)) {
+    const temp = parseFloat(e.value);
+    
+    if (isNaN(temp)) {
       this.setState({ verdict: '' });
       return;
     }
 
-    const notOrEmptyString = e.celsius < 100 ? "not" : "";
+    const temperature = this.scale === "c" ? e.value : toCelsius(e.value);
+
+    const notOrEmptyString = temperature < 100 ? "not" : "";
 
     this.setState({ verdict: `The water would ${notOrEmptyString} boil` });
   };
@@ -77,7 +80,7 @@ class BoilingVerdict extends React.Component {
   constructor(props) {
     super(props);
     this.state = { verdict: '' };
-    subscribe(TemperatureInput.eventName, this.onTemperatureChanged);
+    subscribe(temperatureChanged, this.onTemperatureChanged);
   }
 }
 
